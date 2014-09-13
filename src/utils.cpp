1b9a62326d2f5c88c7f8b3c440addb1011138376
Hamdi Sahloul <hamdisahloul@hotmail.com>
2014-09-14 05:56:22 +0900

    Fixed the rotation of coordinates to match the camera and ROS specifications

--- src/utils.cpp
+++ src/utils.cpp
@@ -13,59 +13,66 @@
 #include <opencv2/calib3d/calib3d.hpp>
 
 aruco::CameraParameters ar_sys::getCamParams(const sensor_msgs::CameraInfo& cam_info,
-                                                                bool useRectifiedParameters)
+	bool useRectifiedParameters)
 {
-    cv::Mat cameraMatrix(3, 3, CV_32FC1);
-    cv::Mat distorsionCoeff(4, 1, CV_32FC1);
-    cv::Size size(cam_info.height, cam_info.width);
+	cv::Mat cameraMatrix(3, 3, CV_32FC1);
+	cv::Mat distorsionCoeff(4, 1, CV_32FC1);
+	cv::Size size(cam_info.height, cam_info.width);
 
-    if ( useRectifiedParameters )
-    {
-      cameraMatrix.setTo(0);
-      cameraMatrix.at<float>(0,0) = cam_info.P[0];   cameraMatrix.at<float>(0,1) = cam_info.P[1];   cameraMatrix.at<float>(0,2) = cam_info.P[2];
-      cameraMatrix.at<float>(1,0) = cam_info.P[4];   cameraMatrix.at<float>(1,1) = cam_info.P[5];   cameraMatrix.at<float>(1,2) = cam_info.P[6];
-      cameraMatrix.at<float>(2,0) = cam_info.P[8];   cameraMatrix.at<float>(2,1) = cam_info.P[9];   cameraMatrix.at<float>(2,2) = cam_info.P[10];
+	if ( useRectifiedParameters )
+	{
+		cameraMatrix.setTo(0);
+		cameraMatrix.at<float>(0,0) = cam_info.P[0]; cameraMatrix.at<float>(0,1) = cam_info.P[1]; cameraMatrix.at<float>(0,2) = cam_info.P[2];
+		cameraMatrix.at<float>(1,0) = cam_info.P[4]; cameraMatrix.at<float>(1,1) = cam_info.P[5]; cameraMatrix.at<float>(1,2) = cam_info.P[6];
+		cameraMatrix.at<float>(2,0) = cam_info.P[8]; cameraMatrix.at<float>(2,1) = cam_info.P[9]; cameraMatrix.at<float>(2,2) = cam_info.P[10];
 
-      for(int i=0; i<4; ++i)
-        distorsionCoeff.at<float>(i, 0) = 0;
-    }
-    else
-    {
-      for(int i=0; i<9; ++i)
-        cameraMatrix.at<float>(i%3, i-(i%3)*3) = cam_info.K[i];
+		for(int i=0; i<4; ++i)
+			distorsionCoeff.at<float>(i, 0) = 0;
+	}
+	else
+	{
+		for(int i=0; i<9; ++i)
+			cameraMatrix.at<float>(i%3, i-(i%3)*3) = cam_info.K[i];
 
-      for(int i=0; i<4; ++i)
-        distorsionCoeff.at<float>(i, 0) = cam_info.D[i];
-    }
+		for(int i=0; i<4; ++i)
+			distorsionCoeff.at<float>(i, 0) = cam_info.D[i];
+	}
 
-    return aruco::CameraParameters(cameraMatrix, distorsionCoeff, size);
+	return aruco::CameraParameters(cameraMatrix, distorsionCoeff, size);
 }
 
 tf::Transform ar_sys::getTf(const cv::Mat &Rvec, const cv::Mat &Tvec)
 {
-    cv::Mat rot(3, 3, CV_32FC1);
-    cv::Rodrigues(Rvec, rot);
+	cv::Mat rot(3, 3, CV_32FC1);
+	cv::Rodrigues(Rvec, rot);
 
-    cv::Mat rotate_to_sys(3, 3, CV_32FC1);
-    // -1 0 0
-    // 0 0 1
-    // 0 1 0
-    rotate_to_sys.at<float>(0,0) = -1.0;
-    rotate_to_sys.at<float>(0,1) = 0.0;
-    rotate_to_sys.at<float>(0,2) = 0.0;
-    rotate_to_sys.at<float>(1,0) = 0.0;
-    rotate_to_sys.at<float>(1,1) = 0.0;
-    rotate_to_sys.at<float>(1,2) = 1.0;
-    rotate_to_sys.at<float>(2,0) = 0.0;
-    rotate_to_sys.at<float>(2,1) = 1.0;
-    rotate_to_sys.at<float>(2,2) = 0.0;
-    rot = rot*rotate_to_sys.t();
+	cv::Mat rotate_to_sys(3, 3, CV_32FC1);
+	/**
+	/* Fixed the rotation to meet the ROS system
+	/* Doing a basic rotation around X with theta=PI
+	/* By Sahloul
+	/* See http://en.wikipedia.org/wiki/Rotation_matrix for details
+	*/
 
-    tf::Matrix3x3 tf_rot(rot.at<float>(0,0), rot.at<float>(0,1), rot.at<float>(0,2),
-                       rot.at<float>(1,0), rot.at<float>(1,1), rot.at<float>(1,2),
-                       rot.at<float>(2,0), rot.at<float>(2,1), rot.at<float>(2,2));
+	//	1	0	0
+	//	0	-1	0
+	//	0	0	-1
+	rotate_to_sys.at<float>(0,0) = 1.0;
+	rotate_to_sys.at<float>(0,1) = 0.0;
+	rotate_to_sys.at<float>(0,2) = 0.0;
+	rotate_to_sys.at<float>(1,0) = 0.0;
+	rotate_to_sys.at<float>(1,1) = -1.0;
+	rotate_to_sys.at<float>(1,2) = 0.0;
+	rotate_to_sys.at<float>(2,0) = 0.0;
+	rotate_to_sys.at<float>(2,1) = 0.0;
+	rotate_to_sys.at<float>(2,2) = -1.0;
+	rot = rot*rotate_to_sys.t();
 
-    tf::Vector3 tf_orig(Tvec.at<float>(0,0), Tvec.at<float>(1,0), Tvec.at<float>(2,0));
+	tf::Matrix3x3 tf_rot(rot.at<float>(0,0), rot.at<float>(0,1), rot.at<float>(0,2),
+		rot.at<float>(1,0), rot.at<float>(1,1), rot.at<float>(1,2),
+		rot.at<float>(2,0), rot.at<float>(2,1), rot.at<float>(2,2));
 
-    return tf::Transform(tf_rot, tf_orig);
+	tf::Vector3 tf_orig(Tvec.at<float>(0,0), Tvec.at<float>(1,0), Tvec.at<float>(2,0));
+
+	return tf::Transform(tf_rot, tf_orig);
 }
